bf2f77218a833bf6afa108b77184cec0c486521f
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-07-21 17:05:13 -0500

    Add all logic for counter

--- src/js/script.js
+++ src/js/script.js
@@ -5,17 +5,28 @@ const period = 14;
 let newDate = new Date();
 newDate.setDate(today.getDate() + period);
 
-const secElem = document.querySelector('#seconds');
-console.log(secElem.firstChild);
+const daysElem = document.querySelector('#days');
+const hoursElem = document.querySelector('#hours');
+const minsElem = document.querySelector('#minutes');
+const secsElem = document.querySelector('#seconds');
 
 const counter = setInterval(function () {
   let now = new Date().getTime();
   let difference = newDate.getTime() - now;
+
   let days = Math.floor(difference / (1000 * 60 * 60 * 24));
   let hours = Math.floor(
     (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
   let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((difference % (1000 * 60)) / 1000);
-  console.log(`Time: ${days} days, ${hours}hrs, ${minutes}m, ${seconds}sec. `);
+
+  daysElem.firstElementChild.innerHTML = days;
+  daysElem.lastElementChild.dataset.value = days;
+  hoursElem.firstElementChild.innerHTML = hours;
+  hoursElem.lastElementChild.dataset.value = hours;
+  minsElem.firstElementChild.innerHTML = minutes;
+  minsElem.lastElementChild.dataset.value = minutes;
+  secsElem.firstElementChild.innerHTML = seconds;
+  secsElem.lastElementChild.dataset.value = seconds;
 }, 1000);
